2925c96dcdcbcd4afda8406214cbc498a2034205
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-09-29 09:34:04 -0400

    format

--- RandomVariables.py
+++ RandomVariables.py
@@ -1,12 +1,12 @@
 import operator
 import math
-from typing import Sequence
+from typing import Sequence, Iterable
+import utils
 
-T = Sequence[float]
 class RV:
-  def __init__(self, vals: T, probs: T):
+  def __init__(self, vals: Sequence[float], probs: Sequence[int]):
     zipped = sorted(zip(vals, probs), reverse=True)
-    newzipped: list[tuple[float, float]] = []
+    newzipped: list[tuple[float, int]] = []
     for i in range(len(zipped)-1, -1, -1):
       if i > 0 and zipped[i][0] == zipped[i-1][0]: # add the two probs, go to next
         zipped[i-1] = (zipped[i-1][0], zipped[i-1][1]+zipped[i][1])
@@ -14,6 +14,10 @@ class RV:
         newzipped.append(zipped[i])
     self.vals = tuple(v[0] for v in newzipped)
     self.probs = tuple(v[1] for v in newzipped)
+    assert all(isinstance(p, int) and p >= 0 for p in self.probs), 'probs must be non-negative integers'
+    gcd = math.gcd(*self.probs)
+    self.probs = tuple(p//gcd for p in self.probs)
+
   def mean(self):
     sum_p = sum(self.probs)
     return sum(v*p for v, p in zip(self.vals, self.probs)) / sum_p
@@ -60,7 +64,7 @@ class RV:
     return self.convolve(other, operator.mod)
   def __rmod__(self, other):
     return self.rconvolve(other, operator.mod)
-  
+
   def __eq__(self, other):
     return self.convolve(other, lambda x, y: 1 if x == y else 0)
   def __ne__(self, other):
@@ -73,11 +77,11 @@ class RV:
     return self.convolve(other, lambda x, y: 1 if x > y else 0)
   def __ge__(self, other):
     return self.convolve(other, lambda x, y: 1 if x >= y else 0)
-  
+
   def __bool__(self):
     assert all(v in (0, 1) for v in self.vals)
     return len(self.vals) == 1 and self.vals[0] == 1
-  
+
   def __pos__(self):
     return self
   def __neg__(self):
@@ -92,19 +96,22 @@ class RV:
     return RV(tuple(math.ceil(v) for v in self.vals), self.probs)
   def __trunc__(self):
     return RV(tuple(math.trunc(v) for v in self.vals), self.probs)
-  
+
   def __repr__(self):
     sum_p = sum(self.probs)
     result = f'mean: {self.mean():.2f} std: {self.std():.2f} \n'
     result += '\n'.join(f"{v}: {round(100*p/sum_p, 2)}" for v, p in zip(self.vals, self.probs))
     return result
 
+
 def dice(n):
   if isinstance(n, int):
     if n == 0:  # special case
       return RV([0], [1])
     return RV(list(range(1, n+1)), [1]*n)
-  if isinstance(n, Sequence):
+  if isinstance(n, Iterable):
+    n = list(utils.flatten(n))
+    n = [x for x in n if not isinstance(x, RV)] + [v for x in n if isinstance(x, RV) for v in x.vals]
     return RV(n, [1]*len(n))
   raise ValueError(f'cant get dice from {type(n)}')
 
