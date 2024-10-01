c321c285a0978d13abade8aef9280f9e291e5396
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-09-30 23:38:52 -0400

    implement rest of functions in funclib.py

--- funclib.py
+++ funclib.py
@@ -13,7 +13,7 @@ def contains(SEQUENCE:Seq, NUMBER:int):
     return (SEQUENCE == NUMBER) > 0
 
 @rv.anydice_casting()
-def count_in (VALUES:Seq, SEQUENCE:Seq):
+def count_in(VALUES:Seq, SEQUENCE:Seq):
     COUNT = 0
     for P in range(1, len(VALUES)+1):
         COUNT = COUNT + (P@VALUES == SEQUENCE)
@@ -30,7 +30,7 @@ def reverse(SEQUENCE:Seq):
     return Seq(SEQUENCE.seq[::-1])
 
 @rv.anydice_casting()
-def maximum_of (DIE:RV):
+def maximum_of(DIE:RV):
     return 1@reverse(Seq(DIE))
 
 @rv.anydice_casting()
@@ -45,5 +45,64 @@ def _explode_helper(N:int, MAX:int, ORIG_DIE:RV, depth):
     return N
 
 @rv.anydice_casting()
-def highest_of(NUMBER:int, DICE:RV):
+def highest_N_of_D(NUMBER:int, DICE:RV):
     return Seq(range(1, NUMBER+1))@DICE
+
+@rv.anydice_casting()
+def lowest_N_of_D(NUMBER:int, DICE:RV):
+    return Seq(range((len(DICE) - NUMBER + 1), len(DICE)+1))@DICE
+
+@rv.anydice_casting()
+def middle_N_of_D(NUMBER:int, DICE:RV):
+    if NUMBER == len(DICE): 
+        return DICE
+
+    if NUMBER == 1: 
+        return (1 + (len(DICE) - 1) // 2) @ DICE
+
+    FROM = 1 + (len(DICE) - NUMBER) // 2
+    TO = FROM + NUMBER - 1
+    return Seq(range(FROM, TO+1))@DICE
+
+@rv.anydice_casting()
+def highest_of_N_and_N(A:int, B:int):
+    if A > B:
+        return A
+    return B
+
+@rv.anydice_casting()
+def lowest_of_N_and_N(A:int, B:int):
+    if A < B:
+        return A
+    return B
+
+@rv.anydice_casting()
+def sort_VANILLA_SLOW(SEQUENCE:Seq):
+    SORTED = Seq()
+    for P in range(1, len(SEQUENCE)+1):
+        SORTED = _sort_helper_add_N_to_S(P@SEQUENCE, SORTED)
+    return SORTED
+
+@rv.anydice_casting()
+def _sort_helper_add_N_to_S(N:int, S:Seq):
+    if len(S) == 0:
+        return Seq(N)
+    if N >= 1@S: 
+        return Seq(N, S)
+    if N <= (len(S))@S:
+        return Seq(S, N)
+
+    R = Seq()
+    for P in range(1, len(S)+1):
+        if N >= P@S:
+            R = Seq(R, N, P@S)
+            N = Seq()
+        else: 
+            R = Seq(R, P@S)
+    if len(N): 
+        R = Seq(R, N)
+    return R
+
+@rv.anydice_casting()
+def sort(SEQUENCE:Seq):
+    return Seq(sorted(SEQUENCE, reverse=True))
