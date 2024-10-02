6db739a17e92b456869c5a3a4012cd03ece54c25
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-02 15:11:20 -0400

    remove useless methods in altcast.py

--- src/altcast.py
+++ src/altcast.py
@@ -1,7 +1,4 @@
-
-from typing import Iterable
-
-from randvar import Seq, RV, roll
+from randvar import roll
 
 class __D_BASE:
   def __mul__(self, other):
@@ -15,22 +12,3 @@ class __D_BASE:
     def __mul__(self, other):
       return roll(self.left, other)
 D = __D_BASE()
-
-def dice(n):
-  if isinstance(n, int):
-    if n == 0:  # special case
-      return RV([0], [1])
-    return RV(list(range(1, n+1)), [1]*n)
-  if isinstance(n, Iterable):
-    if not isinstance(n, Seq):
-      n = Seq(*n)
-    return RV(n.seq, [1]*len(n))
-  raise ValueError(f'cant get dice from {type(n)}')
-
-def d(s):
-  #    s is "ndm", example: "4d6"
-  # or s is "dm", example: "d6"
-  s = s.split('d')
-  if s[0] == '':
-    return roll(1, int(s[1]))
-  return roll(int(s[0]), int(s[1]))
