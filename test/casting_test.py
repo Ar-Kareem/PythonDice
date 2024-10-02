b093eee8f506e2dc1c00300d5383b37d79c30165
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-02 13:50:50 -0400

    add test cast

--- test/casting_test.py
+++ test/casting_test.py
@@ -71,3 +71,25 @@ def test_cast_to_seq(s1, s2):
 ])
 def test_cast_to_rv(s1, s2):
     assert RV.dices_are_equal(f3(s1), s2)
+
+def test_cast_multiple_to_seq():
+    @anydice_casting()
+    def count(VALUES:Seq, SEQUENCE:Seq, *args):
+        COUNT = 0
+        for P in range(1, len(VALUES)+1):
+            COUNT = COUNT + (P@VALUES == SEQUENCE)
+        return COUNT
+
+    a = count(VALUES=roll(3, 2), SEQUENCE=roll(2, 2))  # type: ignore
+    b = count(roll(3, 2), SEQUENCE=roll(2, 2))  # type: ignore
+    c = count(roll(3, 2), roll(2, 2), 1, 1, 1)  # type: ignore
+    dd = RV(vals=(0, 2, 3, 4, 6), probs=(1, 3, 8, 3, 1))  # manual
+    assert RV.dices_are_equal(a, b) and RV.dices_are_equal(b, c) and RV.dices_are_equal(c, dd), 'casting multiple dices to sequence'
+
+def test2_cast_to_seq():
+    @anydice_casting()
+    def cast(M:Seq):
+        return M+1
+    a = cast(roll(2, 2))  # type: ignore
+    assert RV.dices_are_equal(a, roll(2, 2)+1), 'casting single dice to sequence'
+
