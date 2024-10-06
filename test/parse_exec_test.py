d8826be99bd8dfb59384572efd00e9a0707ec225
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-06 03:45:24 -0400

    add tests for parsing ands and ors

--- test/parse_exec_test.py
+++ test/parse_exec_test.py
@@ -8,6 +8,20 @@ from parser.parse_and_exec import pipeline
 settings_set('RV_IGNORE_ZERO_PROBS', True)
 bv = (0, 1)
 
+def check(x, expected):
+    print(x)
+    assert type(x) == type(expected), f'types expected {type(expected)} got {type(x)}'
+    if not isinstance(x, tuple):
+        x = [x]
+        expected = [expected]
+    for a, b in zip(x, expected):
+        if isinstance(a, RV):
+            assert a.vals == b.vals, f'expected vals {b.vals} got {a.vals}'
+            assert a.probs == b.probs, f'expected probs {b.probs} got {a.probs}'
+        else:
+            assert a == b, f'expected {b} got {a}'
+
+
 @pytest.mark.parametrize("code,res", [
 ('''
 output ((1d4-1) | (1d2-1))
@@ -24,23 +38,24 @@ output {2}&{2,-1}
 ),('''
 output {2,-2}&{2,-2}
 ''', [0]
+),('''
+output {1,2,3}&1
+''', [1]
+),('''
+output 1d2&1d2
+''', [RV(bv, (0, 1))]
+),('''
+output {1}&1d2
+''', [RV(bv, (0, 1))]
+),('''
+output 1d2&{1}
+''', [RV(bv, (0, 1))]
 ),])
-def test_parse_and_exec(code, res):
+def test_ands_and_ors(code, res):
     i = 0
     def check_res(x):
         nonlocal i
-        expected = res[i]
-        print(x)
-        assert type(x) == type(expected), f'types expected {type(expected)} got {type(x)}'
-        if not isinstance(x, tuple):
-            x = [x]
-            expected = [expected]
-        for a, b in zip(x, expected):
-            if isinstance(a, RV):
-                assert a.vals == b.vals, f'expected vals {b.vals} got {a.vals}'
-                assert a.probs == b.probs, f'expected probs {b.probs} got {a.probs}'
-            else:
-                assert a == b, f'expected {b} got {a}'
+        check(x, res[i])
         i += 1
     pipeline(code, global_vars={'output': lambda x: check_res(x)})
 
