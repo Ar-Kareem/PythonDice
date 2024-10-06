ef40f0032c4b2bd77d687305ff606f08fe9767ff
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-05 23:56:14 -0400

    lex/yacc resolve not operator

--- src/parser/python_resolver.py
+++ src/parser/python_resolver.py
@@ -118,6 +118,8 @@ class PythonResolver:
                 return f'{self.resolve_node(left)} {op} {self.resolve_node(right)}'
         elif node[0] == 'unary':
             op, expr = node[1:]
+            if op == '!':
+                return f'(not {self.resolve_node(expr)})'
             return f'{op}{self.resolve_node(expr)}'
         elif node[0] == 'hash':  # len
             return f'len({self.resolve_node(node[1])})'
