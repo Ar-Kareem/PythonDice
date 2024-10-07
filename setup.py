db649dfd75323643fe3bdf645dc32204964f5a46
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-07 17:03:34 -0400

    setup for packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pythondice',
+        version='0.0.1',
+        description='Advanced Calculator for Dice',
+        author='arkareem',
+        packages=['distutils', 'distutils.command'],
+    )
