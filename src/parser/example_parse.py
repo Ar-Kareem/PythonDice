890d3af55a8d59303e84a9fad410cb813c3d418b
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-06 20:42:36 -0400

    improve example_parse.py module

--- src/parser/example_parse.py
+++ src/parser/example_parse.py
@@ -1,6 +1,21 @@
 
 trials = [
 r'''
+function: test {
+  result: 1
+}
+function: another test D:d {
+  result: 1
+}
+
+
+\function: anotheranother test D {
+  result: 1
+}\
+
+function: beast {result: [test]}
+output [beast]
+output 2*2
 output [absolute 1]
 output [{1..5} contains 1]
 output [count 1 in {1..5}]
@@ -13,24 +28,32 @@ output [lowest of 1 and 1]
 output [maximum of 2d3]
 output [reverse {1..4}]
 output [sort {1..4}]
+loop P over {1..3} {
+  output P
+}
+A__A: 1
+output 999
 '''
 ]
 
 import logging
 from .parse_and_exec import pipeline
+from randvar import output
 
 def main():
   for to_parse in trials:
     try:
-      pipeline(to_parse, 
+      r = pipeline(to_parse, 
                     do_exec=True, 
                     verbose_input_str=False, 
                     verbose_lex=False, 
                     verbose_yacc=False, 
                     verbose_parseed_python=True,
-                    global_vars={'output': lambda x, named=None: 1}
+                    # global_vars={'output': lambda x, named=None: 1}
                     # global_vars={'output': lambda x, named=None: print('output called', named)}
                     )
+      for (args, kwargs) in r:
+        output(*args, **kwargs)
     except Exception as e:
       logging.warning(f'Error in parsing: {to_parse}')
       logging.exception(e)
