13354fe52dc20527aaf35f55d839d7b1805ea198
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-07 20:15:51 -0400

    properly add parser.ply for lex/yacc to work in published package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='dice_calc',
-    version='0.0.8',
+    version='0.0.9',
     author='Ar-Kareem',
     description='Advanced Calculator for Dice',
     package_dir={
@@ -12,8 +12,14 @@ setup(
         'dice_calc': 'src',
         # parser 'src/parser'
         'dice_calc.parser': 'src/parser',
+        # ply 'src/parser/ply'
+        'dice_calc.parser.ply': 'src/parser/ply',
     },
-    packages=['dice_calc', 'dice_calc.parser'],
+    packages=['dice_calc', 'dice_calc.parser', 'dice_calc.parser.ply'],
 
 )
-#            rm dist/* && python3 setup.py sdist && python3 -m twine upload --repository pypi dist/*
+#build/public            rm dist/* && python3 setup.py sdist && python3 -m twine upload --repository pypi dist/*
+
+
+
+# test locally                 pip uninstall -y dice_calc ; pip install ..\..\..\pythondice\dist\dice_calc-0.0.9.tar.gz ; python ./test.py
