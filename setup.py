c05108db546252721966f5e2c09031fce9a0fa17
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-07 20:07:36 -0400

    properly add package 'parser' to setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from setuptools import setup
 
 setup(
     name='dice_calc',
-    version='0.0.4',
+    version='0.0.8',
     author='Ar-Kareem',
     description='Advanced Calculator for Dice',
-    package_dir={'dice_calc': 'src'},
-    packages=['dice_calc'],    
-
-
+    package_dir={
+        # main package 'src'
+        'dice_calc': 'src',
+        # parser 'src/parser'
+        'dice_calc.parser': 'src/parser',
+    },
+    packages=['dice_calc', 'dice_calc.parser'],
 
 )
+#            rm dist/* && python3 setup.py sdist && python3 -m twine upload --repository pypi dist/*
