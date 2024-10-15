36d231e521ea40613d527d4a364d15bc1b02e3cb
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-15 08:54:08 -0400

    version 2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='dice_calc',
-    version='0.1.9',
+    version='0.2.0',
     author='Ar-Kareem',
     description='Advanced Calculator for Dice',
     long_description=long_description,
