7bf17f09f4faf99aa168f1989f6f1b49251a3826
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-17 09:15:35 -0400

    update __init__.py for easy package usability

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+
+# core classes
+from .randvar import RV, Seq
+
+# core decorators
+from .randvar import anydice_casting, max_func_depth
+
+# core functions
+from .randvar import output, roll, settings_set, settings_reset
